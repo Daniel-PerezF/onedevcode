1bf5e4e820fb08cd5e5494d63fa2d04eb060b452
Daniel <danielperezf98@gmail.com>
2024-07-30 20:40:13 -0700

    css

---
+++ onedevcode/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="w-full">
+      <div className="w-full flex justify-center text-4xl items-center h-screen ">
+        <a href="/" className="underline-center-outward slide-under">
+          Baked beans
+        </a>
+      </div>
+      <div className="blob"></div>
+    </div>
+  );
+}
+
+export default App;
